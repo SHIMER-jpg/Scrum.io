ca457899378c87d6439be4b0cf2f2044bae0d445
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-08-30 16:19:19 -0300

    routes index.js done, and added hardcoded data TODOS

---
+++ api/src/routes/hardcodingDataBD.js
@@ -0,0 +1,39 @@
+const TODO_1 = JSON.parse(`{
+							"id":"1",
+							"title":"User Story 1", 
+							"description":"como desarrollador, quiero poder desarrollar",
+							"storypoint":"5"
+							"complexitymatrix": "easy win"
+						}`);
+const TODO_2 = JSON.parse(`{
+							"id":"2",
+							"title":"User Story 2", 
+							"description":"como scrummaster, quiero legislar leyes de regulacion laboral",
+							"storypoint":"15"
+							"complexitymatrix": "worth persuing later"
+						}`);
+const TODO_3 = JSON.parse(`{
+							"id":"3",
+							"title":"User Story 3", 
+							"description":"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
+							"storypoint":"50"
+							"complexitymatrix": "desprioritize"
+						}`);
+const TODO_4 = JSON.parse(`{
+							"id":"4",
+							"title":"User Story 4", 
+							"description":"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
+							"storypoint":"1"
+							"complexitymatrix": "easy win"
+						}`);
+const TODO_5 = JSON.parse(`{
+							"id":"5",
+							"title":"User Story 5", 
+							"description":"como desarrollador, quiero poder evadir impuestos",
+							"storypoint":"100"
+							"complexitymatrix": "desprioritize"
+						}`);
+
+const TODOS = [TODO_1, TODO_2, TODO_3, TODO_4, TODO_5];
+
+export default TODOS;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,1 +1,13 @@
 //objeto de ruteo
+import { Router } from 'express';
+
+// routes:
+import helpTasks from './helpTasks';
+import toDos from './toDos';
+
+const router = Router();
+
+router.use('/helpTasks', helpTasks);
+router.use('/toDos', toDos);
+
+export default router;
