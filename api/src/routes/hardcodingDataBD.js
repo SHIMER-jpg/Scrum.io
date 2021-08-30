e452db0e10c6a7dea16f1380d59f84dbc7eb6644
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-08-30 16:49:14 -0300

    hardcoded examples modified

--- api/src/routes/hardcodingDataBD.js
+++ api/src/routes/hardcodingDataBD.js
@@ -1,39 +1,106 @@
-const TODO_1 = JSON.parse(`{
-							"id":"1",
-							"title":"User Story 1", 
-							"description":"como desarrollador, quiero poder desarrollar",
-							"storypoint":"5"
-							"complexitymatrix": "easy win"
-						}`);
-const TODO_2 = JSON.parse(`{
-							"id":"2",
-							"title":"User Story 2", 
-							"description":"como scrummaster, quiero legislar leyes de regulacion laboral",
-							"storypoint":"15"
-							"complexitymatrix": "worth persuing later"
-						}`);
-const TODO_3 = JSON.parse(`{
-							"id":"3",
-							"title":"User Story 3", 
-							"description":"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
-							"storypoint":"50"
-							"complexitymatrix": "desprioritize"
-						}`);
-const TODO_4 = JSON.parse(`{
-							"id":"4",
-							"title":"User Story 4", 
-							"description":"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
-							"storypoint":"1"
-							"complexitymatrix": "easy win"
-						}`);
-const TODO_5 = JSON.parse(`{
-							"id":"5",
-							"title":"User Story 5", 
-							"description":"como desarrollador, quiero poder evadir impuestos",
-							"storypoint":"100"
-							"complexitymatrix": "desprioritize"
-						}`);
+const PROJECT_1{
+	name: "Proyecto 1",
+	todoList:[
+		TODO_1: {
+			id:1,
+			title:"User Story 1", 
+			description:"como desarrollador, quiero poder desarrollar",
+			storypoint:5
+			complexitymatrix: "easy win",
+			userId:12,
+			help:true
+		},
+		TODO_2: {
+			id:"2",
+			title:"User Story 2", 
+			description:"como scrummaster, quiero legislar leyes de regulacion laboral",
+			storypoint:"15"
+			complexitymatrix: "worth persuing later",
+			userId:12,
+			help:false
+		},
+		TODO_3: {
+			id:"3",
+			title:"User Story 3", 
+			description:"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
+			storypoint:"50"
+			complexitymatrix: "desprioritize",
+			userId:20,
+			help:true
+		},
+		TODO_4: {
+			id:"4",
+			title:"User Story 4", 
+			description:"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
+			storypoint:"1"
+			complexitymatrix: "easy win",
+			userId:20,
+			help:false
+		},
+		TODO_5: {
+			id:"5",
+			title:"User Story 5", 
+			description:"como desarrollador, quiero poder evadir impuestos",
+			storypoint:"100"
+			complexitymatrix: "desprioritize",
+			userId:20,
+			help:false
+		}
+	]
+}
 
-const TODOS = [TODO_1, TODO_2, TODO_3, TODO_4, TODO_5];
+const PROJECT_2{
+	name: "Proyecto 2",
+	todoList:[
+		TODO_1: {
+			id:1,
+			title:"User Story 1", 
+			description:"como desarrollador, quiero poder desarrollar",
+			storypoint:5
+			complexitymatrix: "easy win",
+			userId:12,
+			help:true
+		},
+		TODO_2: {
+			id:"2",
+			title:"User Story 2", 
+			description:"como scrummaster, quiero legislar leyes de regulacion laboral",
+			storypoint:"15"
+			complexitymatrix: "worth persuing later",
+			userId:12,
+			help:false
+		},
+		TODO_3: {
+			id:"3",
+			title:"User Story 3", 
+			description:"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
+			storypoint:"50"
+			complexitymatrix: "desprioritize",
+			userId:20,
+			help:true
+		},
+		TODO_4: {
+			id:"4",
+			title:"User Story 4", 
+			description:"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
+			storypoint:"1"
+			complexitymatrix: "easy win",
+			userId:20,
+			help:false
+		},
+		TODO_5: {
+			id:"5",
+			title:"User Story 5", 
+			description:"como desarrollador, quiero poder evadir impuestos",
+			storypoint:"100"
+			complexitymatrix: "desprioritize",
+			userId:20,
+			help:false
+		}
+	]
+}
 
-export default TODOS;
+
+const PROJECTS = [PROJECT_1, PROJECT_2];
+
+export default PROJECTS;
