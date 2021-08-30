2ea9af72b7e7263c9a7114a60e4b9b28766dea66
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-08-30 17:13:51 -0300

    hardcoded examples edited

--- api/src/routes/hardcodingDataBD.js
+++ api/src/routes/hardcodingDataBD.js
@@ -1,47 +1,48 @@
-const PROJECT_1{
+const PROJECT_1 = {
+	id:1,
 	name: "Proyecto 1",
 	todoList:[
 		TODO_1: {
 			id:1,
 			title:"User Story 1", 
 			description:"como desarrollador, quiero poder desarrollar",
-			storypoint:5
+			storypoint:5,
 			complexitymatrix: "easy win",
 			userId:12,
 			help:true
 		},
 		TODO_2: {
-			id:"2",
+			id:2,
 			title:"User Story 2", 
 			description:"como scrummaster, quiero legislar leyes de regulacion laboral",
-			storypoint:"15"
+			storypoint:15,
 			complexitymatrix: "worth persuing later",
 			userId:12,
 			help:false
 		},
 		TODO_3: {
-			id:"3",
+			id:3,
 			title:"User Story 3", 
 			description:"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
-			storypoint:"50"
+			storypoint:50,
 			complexitymatrix: "desprioritize",
 			userId:20,
 			help:true
 		},
 		TODO_4: {
-			id:"4",
+			id:4,
 			title:"User Story 4", 
 			description:"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
-			storypoint:"1"
+			storypoint:1,
 			complexitymatrix: "easy win",
 			userId:20,
 			help:false
 		},
 		TODO_5: {
-			id:"5",
+			id:5,
 			title:"User Story 5", 
 			description:"como desarrollador, quiero poder evadir impuestos",
-			storypoint:"100"
+			storypoint:100,
 			complexitymatrix: "desprioritize",
 			userId:20,
 			help:false
@@ -49,50 +50,51 @@ const PROJECT_1{
 	]
 }
 
-const PROJECT_2{
+const PROJECT_2 = {
+	id:2,
 	name: "Proyecto 2",
 	todoList:[
 		TODO_1: {
 			id:1,
 			title:"User Story 1", 
 			description:"como desarrollador, quiero poder desarrollar",
-			storypoint:5
+			storypoint:5,
 			complexitymatrix: "easy win",
 			userId:12,
 			help:true
 		},
 		TODO_2: {
-			id:"2",
+			id:2,
 			title:"User Story 2", 
 			description:"como scrummaster, quiero legislar leyes de regulacion laboral",
-			storypoint:"15"
+			storypoint:15,
 			complexitymatrix: "worth persuing later",
 			userId:12,
 			help:false
 		},
 		TODO_3: {
-			id:"3",
+			id:3,
 			title:"User Story 3", 
 			description:"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
-			storypoint:"50"
+			storypoint:50,
 			complexitymatrix: "desprioritize",
 			userId:20,
 			help:true
 		},
 		TODO_4: {
-			id:"4",
+			id:4,
 			title:"User Story 4", 
 			description:"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
-			storypoint:"1"
+			storypoint:1,
 			complexitymatrix: "easy win",
 			userId:20,
 			help:false
 		},
 		TODO_5: {
-			id:"5",
+			id:5,
 			title:"User Story 5", 
 			description:"como desarrollador, quiero poder evadir impuestos",
-			storypoint:"100"
+			storypoint:100,
 			complexitymatrix: "desprioritize",
 			userId:20,
 			help:false
