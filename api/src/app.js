9d520ead3077fe350d312335411daa3b22b438c9
Lucero Amaolo <lamaolo.m@gmail.com>
2021-08-30 15:05:43 -0300

    Added response.js

--- api/src/app.js
+++ api/src/app.js
@@ -29,13 +29,4 @@ server.use((req, res, next) => {
 
 // server.use("/", routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
 module.exports = server;
