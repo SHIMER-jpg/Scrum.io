509ea13667c939c8deffe4aafb36c9590d2dc988
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-08-31 16:15:35 -0300

    Todo and TodoCard edited for styles

--- client/src/components/TodoCard.js
+++ client/src/components/TodoCard.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import styles from '../styles/TodoCard.module.css';
 
 export default function TodoCard({name, description, sp, complex}) {
+  complex = complex.replaceAll(" ", "_");
+
   return (
-    <div className="TodoCard">
-      <div className="TodoCard-Header">
+    <div className={styles.TodoCard}>
+      <div className={styles.TodoCard_Header}>
         <h3>{name}</h3>
+        <span className={styles.TodoCard_StoryPoints}>{sp} SP</span>
       </div>
-      <div className="TodoCard-Body">
-        <span className="TodoCard-StoryPoints">{sp}</span>
+      <div className={styles.TodoCard_Body}>
         <p>{description}</p>
       </div>
-      <div className="TodoCard-ComplexColorLine">
-        {complex}
-      </div>
+      <div className={`${styles.TodoCard_ComplexColorLine} ${complex === "easy_win" ? styles.easy_win 
+      : complex === "worth_persuing_later" ? styles.worth_persuing_later 
+      : complex === "desprioritize" ? styles.desprioritize : styles.strategic_initiatives}`}></div>
     </div>
   );
 }
--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import styles from '../styles/TodoCard.module.css';
 
 //components
 import TodoCard from './TodoCard.js';
 
+// coso
+import PROJECTS from '../hardcodingDataBD';
+
 export default function Todos() {
   return (
-    <div className="Todos">
-      <div className="Todos-Header">
-        <h1>To Do</h1>
+    <div className={styles.Todos}>
+      <div className={styles.Todos_Header}>
+        <h2>To Do</h2>
       </div>
-      <div className="TodoList">
-        <TodoCard 
-          name={"Task Name"}
-          description={"HolaHola, soy una descripcion de TODO"}
-          sp={"99"}
-          complex={"easy win"}
-        />
+      <div className={styles.TodoList}>
+        {PROJECTS.length > 0 && PROJECTS[0].todoList.map(pro => <TodoCard 
+          name={pro.title}
+          description={pro.description}
+          sp={pro.storypoint}
+          complex={pro.complexitymatrix}
+        />)}
       </div>
     </div>
   );
