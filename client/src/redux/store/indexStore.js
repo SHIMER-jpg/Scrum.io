7a58219f91ceacd77e4b8f1c20df22861734a60d
SHIMER-jpg <73915679+SHIMER-jpg@users.noreply.github.com>
2021-09-01 11:49:57 -0300

    front end boilerplate re-hecho

--- client/src/redux/store/indexStore.js
+++ client/src/redux/store/indexStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import rootReducer from './rootReducer';
+import rootReducer from '../rootReducer';
 
 const store = create(
     rootReducer,
