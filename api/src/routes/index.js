15b28d7e62d3c0da7f288c590fc03da8cc25201b
SHIMER-jpg <73915679+SHIMER-jpg@users.noreply.github.com>
2021-09-01 12:09:53 -0300

    back end imports refactorizados

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,11 @@
 //objeto de ruteo
-const { Router } = require('express');
+const { Router } = require("express");
 
 // routes:
-const todoHelp = require('./todoHelp');
-const todos = require('./todos');
+const task = require("./task");
 
 const router = Router();
 
-router.use('/todoHelp', todoHelp);
-router.use('/todos', todos);
+router.use("/task", task);
 
 module.exports = router;
