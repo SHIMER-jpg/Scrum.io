780402af55fc21c817d92e114ab4307007c7029b
Lucero Amaolo <lamaolo.m@gmail.com>
2021-09-01 18:34:10 -0300

    Terminado homepage

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,9 +1,22 @@
+import { RiHomeLine } from "react-icons/ri";
+import { BsGear } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
+
 import styles from "./Sidebar.module.css";
 
 const Sidebar = () => {
   return (
     <nav className={styles.container}>
-      <h2>sidebar</h2>
+      <div className={styles.item}>
+        <NavLink to="/home">
+          <RiHomeLine size={23} /> Home
+        </NavLink>
+      </div>
+      <div className={styles.item}>
+        <NavLink to="/configuration">
+          <BsGear size={23} /> Configuration
+        </NavLink>
+      </div>
     </nav>
   );
 };
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,11 +1,46 @@
+import { BsPlus } from "react-icons/bs";
+
 import styles from "./Home.module.css";
 
 const DeveloperView = () => {
   return (
-    <section className={styles.header}>
+    <section className={styles.container}>
       <header className={styles.header}>
-        <h1 className={styles.heading}>My projects</h1>
+        <h1>My projects</h1>
+        <button>
+          <BsPlus size={20} /> Create project
+        </button>
       </header>
+      <main className={styles.projects}>
+        <article className={styles.project}>
+          <h2>Proyecto grupal</h2>
+          <div className={styles.projectItem}>
+            <div className={styles.projectItemTitle}>
+              <p>Description</p>
+              <div></div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              similique totam quibusdam officiis! Provident excepturi ad
+              deserunt illum exercitationem in minima beatae laborum, molestiae
+              iure omnis. Officiis perspiciatis nobis voluptatem.
+            </p>
+          </div>
+          <div className={styles.projectItem}>
+            <div className={styles.projectItemTitle}>
+              <p>Progress</p>
+              <div></div>
+            </div>
+            <div className={styles.progress}>
+              <div></div>
+              <p>75%</p>
+            </div>
+          </div>
+          <div className={styles.projectItem}>
+            <button>See details</button>
+          </div>
+        </article>
+      </main>
     </section>
   );
 };
