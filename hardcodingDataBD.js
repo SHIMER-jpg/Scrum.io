436cb8158c077b5343bdfe196578d05bc01188d5
SHIMER-jpg <73915679+SHIMER-jpg@users.noreply.github.com>
2021-09-01 12:13:08 -0300

    cambiado el import en hardcodingdatabd

--- hardcodingDataBD.js
+++ hardcodingDataBD.js
@@ -1,108 +1,113 @@
 const PROJECT_1 = {
-	id:1,
-	name: "Proyecto 1",
-	todoList:[
-		{
-			id:1,
-			title:"User Story 1", 
-			description:"como desarrollador, quiero poder desarrollar",
-			storypoint:5,
-			complexitymatrix: "easy win",
-			userId:12,
-			help:true
-		},
-		{
-			id:2,
-			title:"User Story 2", 
-			description:"como scrummaster, quiero legislar leyes de regulacion laboral",
-			storypoint:15,
-			complexitymatrix: "strategic initiatives",
-			userId:12,
-			help:false
-		},
-		{
-			id:3,
-			title:"User Story 3", 
-			description:"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
-			storypoint:50,
-			complexitymatrix: "desprioritize",
-			userId:20,
-			help:true
-		},
-		{
-			id:4,
-			title:"User Story 4", 
-			description:"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
-			storypoint:1,
-			complexitymatrix: "easy win",
-			userId:20,
-			help:false
-		},
-		{
-			id:5,
-			title:"User Story 5", 
-			description:"como desarrollador, quiero poder evadir impuestos",
-			storypoint:100,
-			complexitymatrix: "worth_persuing_later",
-			userId:20,
-			help:false
-		}
-	]
-}
+  id: 1,
+  name: "Proyecto 1",
+  todoList: [
+    {
+      id: 1,
+      title: "User Story 1",
+      description: "como desarrollador, quiero poder desarrollar",
+      storypoint: 5,
+      complexitymatrix: "easy win",
+      userId: 12,
+      help: true,
+    },
+    {
+      id: 2,
+      title: "User Story 2",
+      description:
+        "como scrummaster, quiero legislar leyes de regulacion laboral",
+      storypoint: 15,
+      complexitymatrix: "strategic initiatives",
+      userId: 12,
+      help: false,
+    },
+    {
+      id: 3,
+      title: "User Story 3",
+      description:
+        "como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
+      storypoint: 50,
+      complexitymatrix: "desprioritize",
+      userId: 20,
+      help: true,
+    },
+    {
+      id: 4,
+      title: "User Story 4",
+      description:
+        "como scrummaster, quiero poder cometer crimenes de lesa humanidad",
+      storypoint: 1,
+      complexitymatrix: "easy win",
+      userId: 20,
+      help: false,
+    },
+    {
+      id: 5,
+      title: "User Story 5",
+      description: "como desarrollador, quiero poder evadir impuestos",
+      storypoint: 100,
+      complexitymatrix: "worth_persuing_later",
+      userId: 20,
+      help: false,
+    },
+  ],
+};
 
 const PROJECT_2 = {
-	id:2,
-	name: "Proyecto 2",
-	todoList:[
-		{
-			id:1,
-			title:"User Story 1", 
-			description:"como desarrollador, quiero poder desarrollar",
-			storypoint:5,
-			complexitymatrix: "easy win",
-			userId:12,
-			help:true
-		},
-		{
-			id:2,
-			title:"User Story 2", 
-			description:"como scrummaster, quiero legislar leyes de regulacion laboral",
-			storypoint:15,
-			complexitymatrix: "strategic initiatives",
-			userId:12,
-			help:false
-		},
-		{
-			id:3,
-			title:"User Story 3", 
-			description:"como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
-			storypoint:50,
-			complexitymatrix: "desprioritize",
-			userId:20,
-			help:true
-		},
-		{
-			id:4,
-			title:"User Story 4", 
-			description:"como scrummaster, quiero poder cometer crimenes de lesa humanidad",
-			storypoint:1,
-			complexitymatrix: "easy win",
-			userId:20,
-			help:false
-		},
-		{
-			id:5,
-			title:"User Story 5", 
-			description:"como desarrollador, quiero poder evadir impuestos",
-			storypoint:100,
-			complexitymatrix: "worth_persuing_later",
-			userId:20,
-			help:false
-		}
-	]
-}
-
+  id: 2,
+  name: "Proyecto 2",
+  todoList: [
+    {
+      id: 1,
+      title: "User Story 1",
+      description: "como desarrollador, quiero poder desarrollar",
+      storypoint: 5,
+      complexitymatrix: "easy win",
+      userId: 12,
+      help: true,
+    },
+    {
+      id: 2,
+      title: "User Story 2",
+      description:
+        "como scrummaster, quiero legislar leyes de regulacion laboral",
+      storypoint: 15,
+      complexitymatrix: "strategic initiatives",
+      userId: 12,
+      help: false,
+    },
+    {
+      id: 3,
+      title: "User Story 3",
+      description:
+        "como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
+      storypoint: 50,
+      complexitymatrix: "desprioritize",
+      userId: 20,
+      help: true,
+    },
+    {
+      id: 4,
+      title: "User Story 4",
+      description:
+        "como scrummaster, quiero poder cometer crimenes de lesa humanidad",
+      storypoint: 1,
+      complexitymatrix: "easy win",
+      userId: 20,
+      help: false,
+    },
+    {
+      id: 5,
+      title: "User Story 5",
+      description: "como desarrollador, quiero poder evadir impuestos",
+      storypoint: 100,
+      complexitymatrix: "worth_persuing_later",
+      userId: 20,
+      help: false,
+    },
+  ],
+};
 
 const PROJECTS = [PROJECT_1, PROJECT_2];
 
-export default PROJECTS;
+module.exports = PROJECTS;
