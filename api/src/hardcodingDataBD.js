3da44bc896d6cccc4b4e4880946fcd55d485a3da
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-09-01 15:17:45 -0300

    cambios en el mockup de api

---
+++ api/src/hardcodingDataBD.js
@@ -0,0 +1,123 @@
+const PROJECT_1 = {
+  id: 1,
+  name: "Proyecto 1",
+  taskList: [
+    {
+      id: 1,
+      title: "User Story 1",
+      status:"Pending",
+      description: "como desarrollador, quiero poder desarrollar",
+      storypoint: 5,
+      complexitymatrix: "easy win",
+      userId: 12,
+      helpNeeded: true,
+    },
+    {
+      id: 2,
+      title: "User Story 2",
+      status:"In progress",
+      description:
+        "como scrummaster, quiero legislar leyes de regulacion laboral",
+      storypoint: 15,
+      complexitymatrix: "strategic initiatives",
+      userId: 12,
+      helpNeeded: false,
+    },
+    {
+      id: 3,
+      title: "User Story 3",
+      status:"Completed",
+      description:
+        "como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
+      storypoint: 50,
+      complexitymatrix: "desprioritize",
+      userId: 20,
+      helpNeeded: true,
+    },
+    {
+      id: 4,
+      title: "User Story 4",
+      status:"Testing",
+      description:
+        "como scrummaster, quiero poder cometer crimenes de lesa humanidad",
+      storypoint: 1,
+      complexitymatrix: "easy win",
+      userId: 20,
+      helpNeeded: false,
+    },
+    {
+      id: 5,
+      title: "User Story 5",
+      status:"Pending",
+      description: "como desarrollador, quiero poder evadir impuestos",
+      storypoint: 100,
+      complexitymatrix: "worth_persuing_later",
+      userId: 20,
+      helpNeeded: false,
+    },
+  ],
+};
+
+const PROJECT_2 = {
+  id: 2,
+  name: "Proyecto 2",
+  taskList: [
+    {
+      id: 1,
+      title: "User Story 1",
+      status:"Pending",
+      description: "como desarrollador, quiero poder desarrollar",
+      storypoint: 5,
+      complexitymatrix: "easy win",
+      userId: 12,
+      helpNeeded: true,
+    },
+    {
+      id: 2,
+      title: "User Story 2",
+      status:"In progress",
+      description:
+        "como scrummaster, quiero legislar leyes de regulacion laboral",
+      storypoint: 15,
+      complexitymatrix: "strategic initiatives",
+      userId: 12,
+      helpNeeded: false,
+    },
+    {
+      id: 3,
+      title: "User Story 3",
+      status:"Completed",
+      description:
+        "como desarrollador, quiero vacaciones en miameeeeeeeeeeeeee",
+      storypoint: 50,
+      complexitymatrix: "desprioritize",
+      userId: 20,
+      helpNeeded: true,
+    },
+    {
+      id: 4,
+      title: "User Story 4",
+      status:"Testing",
+      description:
+        "como scrummaster, quiero poder cometer crimenes de lesa humanidad",
+      storypoint: 1,
+      complexitymatrix: "easy win",
+      userId: 20,
+      helpNeeded: false,
+    },
+    {
+      id: 5,
+      title: "User Story 5",
+      status:"Pending",
+      description: "como desarrollador, quiero poder evadir impuestos",
+      storypoint: 100,
+      complexitymatrix: "worth_persuing_later",
+      userId: 20,
+      helpNeeded: false,
+    },
+  ],
+};
+
+const PROJECTS = [PROJECT_1, PROJECT_2];
+
+module.exports = PROJECTS;
