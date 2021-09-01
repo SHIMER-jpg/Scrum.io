2335bada0247e6360586fca5c758bde4cd3700b4
federico666 <fefosonic1@gmail.com>
2021-09-01 15:32:00 -0300

    creada ruta post Task

--- api/src/routes/task.js
+++ api/src/routes/task.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const { getProjectById } = require("../controllers/projects.js");
 const task = Router();
+const { postTask } = require("../controllers/task.js");
 
 // Get project by id.
 task.get("/:projectId", getProjectById);
+
+task.post("/createTask", postTask);
 
 module.exports = task;
