9e87184ca4743f3d11e6f5d68cd60563cffd604e
Lucero Amaolo <lamaolo.m@gmail.com>
2021-09-02 14:23:02 -0300

    Cambios de UI temporales

--- client/src/views/ManagerView/ManagerView.js
+++ client/src/views/ManagerView/ManagerView.js
@@ -53,95 +53,95 @@ export default function ManagerView(props) {
 
   return (
     <>
-      <div>
-        <Modal isOpen={modalOpen}>
-          <div>
-            <span>Entraste a MODALSXd</span>
-            <button onClick={() => setModalOpen(false)}>X</button>
-            <form onSubmit={(e) => handleSubmit(e)}>
-              <div>
-                <label>Title</label>
-                <input
-                  name="title"
-                  type="text"
-                  value={createTask.test}
-                  onChange={(e) => handleInput(e)}
-                ></input>
-              </div>
-              <div>
-                <label>Asigned Dev</label>
-                <input
-                  name="asignedTo"
-                  type="text"
-                  value={createTask.asignedTo}
-                  onChange={(e) => handleInput(e)}
-                ></input>
-              </div>
-              <div>
-                <label>Story Points</label>
-                <input
-                  name="storyPoints"
-                  type="text"
-                  value={createTask.storyPoints}
-                  onChange={(e) => handleInput(e)}
-                ></input>
-              </div>
-              <div>
-                <label>Priorization</label>
-                <select
-                  value={createTask.priorization}
-                  name="priorization"
-                  onChange={(e) => handleInput(e)}
-                >
-                  <option value="none">None</option>
-                  <option value="Easy Win">Easy Win</option>
-                  <option value="Depriorize">Depriorize</option>
-                  <option value="Worth Pursuing">Worth Pursuing</option>
-                  <option value="Strategic Initiative">
-                    Strategic Initiative
-                  </option>
-                </select>
-              </div>
-              <div>
-                <label>Detail</label>
-                <textarea
-                  style={{ resize: "none" }}
-                  value={createTask.details}
-                  name="details"
-                  onChange={(e) => handleInput(e)}
-                ></textarea>
-              </div>
-              <button type="submit">Create</button>
-            </form>
-          </div>
-        </Modal>
-        <div className={managerStyle.conteiner}>
-          <div className={managerStyle.conteinerHeader}>
-            <h1>Project Name</h1>
-            <button onClick={() => setModalOpen(true)}>Create Task +</button>
-          </div>
-          <div className={managerStyle.conteinerBody}>
-            {/* Pending Tasks */}
-            <TaskHolder
-              status={"Pending"}
-              tasks={tasks.filter((task) => task.status == "Pending")}
-            />
-            {/* In progress Tasks */}
-            <TaskHolder
-              status={"In progress"}
-              tasks={tasks.filter((task) => task.status == "In progress")}
-            />
-            {/* Testing Tasks */}
-            <TaskHolder
-              status={"Testing"}
-              tasks={tasks.filter((task) => task.status == "Testing")}
-            />
-            {/* Completed Tasks */}
-            <TaskHolder
-              status={"Completed"}
-              tasks={tasks.filter((task) => task.status == "Completed")}
-            />
-          </div>
+      <Modal isOpen={modalOpen}>
+        <div>
+          <span>Entraste a MODALSXd</span>
+          <button onClick={() => setModalOpen(false)}>X</button>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div>
+              <label>Title</label>
+              <input
+                name="title"
+                type="text"
+                value={createTask.test}
+                onChange={(e) => handleInput(e)}
+              ></input>
+            </div>
+            <div>
+              <label>Asigned Dev</label>
+              <input
+                name="asignedTo"
+                type="text"
+                value={createTask.asignedTo}
+                onChange={(e) => handleInput(e)}
+              ></input>
+            </div>
+            <div>
+              <label>Story Points</label>
+              <input
+                name="storyPoints"
+                type="text"
+                value={createTask.storyPoints}
+                onChange={(e) => handleInput(e)}
+              ></input>
+            </div>
+            <div>
+              <label>Priorization</label>
+              <select
+                value={createTask.priorization}
+                name="priorization"
+                onChange={(e) => handleInput(e)}
+              >
+                <option value="none">None</option>
+                <option value="Easy Win">Easy Win</option>
+                <option value="Depriorize">Depriorize</option>
+                <option value="Worth Pursuing">Worth Pursuing</option>
+                <option value="Strategic Initiative">
+                  Strategic Initiative
+                </option>
+              </select>
+            </div>
+            <div>
+              <label>Detail</label>
+              <textarea
+                style={{ resize: "none" }}
+                value={createTask.details}
+                name="details"
+                onChange={(e) => handleInput(e)}
+              ></textarea>
+            </div>
+            <button type="submit">Create</button>
+          </form>
+        </div>
+      </Modal>
+      <div className={managerStyle.conteiner}>
+        <header className={managerStyle.conteinerHeader}>
+          <h1 className="main-heading">Project Name</h1>
+          <button className="btn-primary" onClick={() => setModalOpen(true)}>
+            + Create Task
+          </button>
+        </header>
+        <div className={managerStyle.conteinerBody}>
+          {/* Pending Tasks */}
+          <TaskHolder
+            status={"Pending"}
+            tasks={tasks.filter((task) => task.status === "Pending")}
+          />
+          {/* In progress Tasks */}
+          <TaskHolder
+            status={"In progress"}
+            tasks={tasks.filter((task) => task.status === "In progress")}
+          />
+          {/* Testing Tasks */}
+          <TaskHolder
+            status={"Testing"}
+            tasks={tasks.filter((task) => task.status === "Testing")}
+          />
+          {/* Completed Tasks */}
+          <TaskHolder
+            status={"Completed"}
+            tasks={tasks.filter((task) => task.status === "Completed")}
+          />
         </div>
       </div>
     </>
