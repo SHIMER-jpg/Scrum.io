ab403c1bfc2b4eb18abf18f41c6306454eb813fc
Lucero Amaolo <lamaolo.m@gmail.com>
2021-09-05 19:12:41 -0300

    Home muestra los proyectos ordenados cronologicamente

--- client/src/components/ProjectHolder/ProjectHolder.js
+++ client/src/components/ProjectHolder/ProjectHolder.js
@@ -4,16 +4,16 @@ import styles from "./ProjectHolder.module.css";
 //components
 import ProjectCard from "../ProjectCard/ProjectCard.js";
 
-export default function ProjectHolder({ finished, projectList }) { 
-    // `finished` es un booleano para indicar si los proyectos que se mapearan estan terminados o siguen en curso
+export default function ProjectHolder({ finished, projectList }) {
+  // `finished` es un booleano para indicar si los proyectos que se mapearan estan terminados o siguen en curso
 
-    // esto seria para filtrar los proyectos por terminados o no terminados, esta comentado porque algunos se cargaron mal a la bd y no tienen status
-    // if(finished){
-    //   projectList = projectList.filter(project => project.projects.status === 100);
-    // }
-    // else{
-    //   projectList = projectList.filter(project => project.projects.status < 100);
-    // }
+  // esto seria para filtrar los proyectos por terminados o no terminados, esta comentado porque algunos se cargaron mal a la bd y no tienen status
+  // if(finished){
+  //   projectList = projectList.filter(project => project.projects.status === 100);
+  // }
+  // else{
+  //   projectList = projectList.filter(project => project.projects.status < 100);
+  // }
 
   return (
     <div className={styles.projects}>
@@ -23,12 +23,17 @@ export default function ProjectHolder({ finished, projectList }) {
       </div>
       <div className={styles.projectList}>
         {projectList && projectList.length > 0 ? (
-          projectList.map((project) => (
-            <ProjectCard
-              key={project.projects._id}
-              project={project.projects}
-            />
-          ))
+          projectList
+            .sort(
+              ({ projects: a }, { projects: b }) =>
+                new Date(b.creationDate) - new Date(a.creationDate)
+            )
+            .map((project) => (
+              <ProjectCard
+                key={project.projects._id}
+                project={project.projects}
+              />
+            ))
         ) : (
           <h3>No Projects...</h3>
         )}
