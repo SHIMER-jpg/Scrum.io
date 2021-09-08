df6f3080528bc30be647d8f862d6c5ba9cb230b0
Rocio <rocioreginaottavianelli@gmail.com>
2021-09-08 19:51:57 -0300

    conectando grafico de developer con data real de varios usuarios y agregando estilos

--- client/src/components/StatisticDeveloper/StatisticDeveloper.js
+++ client/src/components/StatisticDeveloper/StatisticDeveloper.js
@@ -1,19 +1,25 @@
-import { React, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import  { Bar, Line } from 'react-chartjs-2';
 import styles from "./StatisticDeveloper.module.css";
 
+import { useSearch } from "../../hooks/useSearch";
+
 export default function StatisticDeveloper(props) {
+  const users = useSelector((state) => state.managerView.asignedUsers);
   const tasks = useSelector((state) => state.managerView.tasks);
-  // console.log(tasks);
   const userTasks = tasks.filter((t) => t.asignedTo === "61377a66c16ac40924f9daa0");
   const completedUserTasks = userTasks.filter((t) => t.completedDate);
-  // console.log(userTasks);
-  // console.log(completedUserTasks);
   const labels = getLabels();
   const userData = getData(getDate("2021-09-25T00:00:00.000Z"), getDate("2021-09-17T00:00:00.000Z"), completedUserTasks);
   console.log(userData);
+
+  const [selectedUsers, setSelectedUsers] = React.useState([]);
+  const [data, setData] = React.useState([]);
+  const [isSelectUsersOpen, setIsSelectUsersOpen] = useState(false);
+  console.log(users, "users");
+  // const [query, setQuery, filteredUsers] = useSearch(users);
 
   //-------FUNCTIONS---------------------------------
   function getDate(date){         //converts date strings into integers
@@ -51,6 +57,24 @@ export default function StatisticDeveloper(props) {
     return data;
   }
 
+  function handleAddUser(user){
+    if (!selectedUsers.includes(user._id)) {
+      setSelectedUsers({
+        ...selectedUsers,
+        selectedUsers: [...selectedUsers, user._id]
+      });
+    }
+    // setQuery("");
+  }
+
+  function handleRemoveUser(user){
+    setSelectedUsers({
+      ...selectedUsers,
+      selectedUsers: selectedUsers.filter((u) => u !== user._id)
+    })
+  }
+  console.log(users, "users");
+  // console.log(filteredUsers, "filteredUsers");
   //-------RETURN-------------------------------------
   return (
     <div className={styles.conteiner}>
@@ -93,6 +117,51 @@ export default function StatisticDeveloper(props) {
           />
 
         </div>
+
+        <div>
+          <input
+            onBlur={() => setIsSelectUsersOpen(false)}
+            onFocus={() => setIsSelectUsersOpen(true)}
+            type="text"
+            name="Users"
+            /*value={query}*/
+            autoComplete="off"
+            /*onChange={(e) => setQuery(e.target.value)}*/
+          />
+        </div>
+        <div
+          className={`${styles.modalSelectUser} ${
+            isSelectUsersOpen ? styles.visible : undefined
+          }`}
+        >
+          {users.length ? (
+            users.map((user) => (
+              <article
+                onClick={() => handleAddUser(user)}
+                key={user._id}
+                className={styles.modalUser}
+              >
+                <p>{user.name}</p>
+              </article>
+            ))
+          ) : (
+            <p>There's no user with that name :(</p>
+          )}
+        </div>
+        <div className={styles.addedUsers}>
+          {users
+            .filter((user) => selectedUsers.includs(user._id))
+            .map((user) => {
+              <article key={user._id} className={styles.addedUsersCard}>
+                <p>{user.name.split(" ")[0]}</p>
+                <button onClick={() => handleRemoveUser(user)}>
+                  x
+                </button>
+              </article>
+            })
+          }
+        </div>
+
         <div className={styles.description}>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
