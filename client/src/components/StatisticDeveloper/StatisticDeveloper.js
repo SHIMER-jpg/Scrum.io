15c69adc38ea88f7f32f97f51459dfe38c883965
Rocio <rocioreginaottavianelli@gmail.com>
2021-09-08 14:39:09 -0300

    ahora el grafico de desarrollador usa tareas reales

--- client/src/components/StatisticDeveloper/StatisticDeveloper.js
+++ client/src/components/StatisticDeveloper/StatisticDeveloper.js
@@ -6,21 +6,52 @@ import styles from "./StatisticDeveloper.module.css";
 
 export default function StatisticDeveloper(props) {
   const tasks = useSelector((state) => state.managerView.tasks);
+  // console.log(tasks);
   const userTasks = tasks.filter((t) => t.asignedTo === "61377a66c16ac40924f9daa0");
+  const completedUserTasks = userTasks.filter((t) => t.completedDate);
   // console.log(userTasks);
-  //debería recuperar las fechas de las tareas completadas, obtener la cantidad de cada dia y sus sp
+  // console.log(completedUserTasks);
+  const labels = getLabels();
+  const userData = getData(getDate("2021-09-25T00:00:00.000Z"), getDate("2021-09-17T00:00:00.000Z"), completedUserTasks);
+  console.log(userData);
 
-  function calculateDays(){
-    let finalDate = parseInt("2021-09-25T00:00:00.000Z".slice(0, 10).replaceAll("-", ""));
-    let initialDate = parseInt("2021-09-07T17:26:13.094Z".slice(0, 10).replaceAll("-", ""));
-    let days = finalDate - initialDate;
-    return days;
+  //-------FUNCTIONS---------------------------------
+  function getDate(date){         //converts date strings into integers
+    return parseInt(date.slice(0, 10).replaceAll("-", ""));
   }
 
-  function getLabels(){
-    return "";
+  function calculateDays(){        //calculates x axis values dinamically (in days)
+    let finalDate = getDate("2021-09-25T00:00:00.000Z");
+    let initialDate = getDate("2021-09-07T17:26:13.094Z");
+    return finalDate - initialDate;
   }
 
+  function getLabels(){           //creates an array with the calculated values to be set as labels
+    var days = calculateDays();
+    var labelArray = [];
+    for(let i=0; i<days; i++){
+      labelArray.push(i+1);
+    }
+    return labelArray;
+  }
+
+  function getData(finalDate, actualDate, tasks){  //creates a totalizer data object
+    var days = calculateDays();
+    var data = {quantity: [], storyPoints: []};
+    for(let i=0; i<(finalDate - actualDate); i++){ //initializing both arrays in 0 for each position
+      data.quantity.push(0);
+      data.storyPoints.push(0);
+    }
+    tasks.forEach((t) =>    //mapping user tasks
+      {
+        data.quantity[finalDate - getDate(t.completedDate)]++   //counts completed task quantity
+        data.storyPoints[finalDate - getDate(t.completedDate)] += t.storyPoints  //counts completed story points
+      }
+    )
+    return data;
+  }
+
+  //-------RETURN-------------------------------------
   return (
     <div className={styles.conteiner}>
       <div>
@@ -28,39 +59,23 @@ export default function StatisticDeveloper(props) {
           <h2>Developer Statistics</h2>
         </div>
         <div className={styles.graph}>
-          
+
           <Bar className={styles.chart}
             data={{
-              labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
+              labels: labels,
               datasets: [{
                   type: 'bar',
                   label: 'Completed Tasks',
                   borderColor: 'rgb(54, 162, 235)',
                   borderWidth: 2,
                   backgroundColor: 'rgb(54, 162, 235)',
-                  data: [1, 1, 2, 2],
+                  data: userData.quantity,
                 },
                 {
                   type: 'line',
                   label: 'Story Points',
                   backgroundColor: 'rgb(255, 99, 132)',
-                  data: [12, 3, 15, 8],
-                  borderColor: 'rgb(255, 99, 132)',
-                  borderWidth: 4,
-                },
-                {
-                  type: 'bar',
-                  label: 'Completed Tasks',
-                  borderColor: 'rgb(54, 162, 235)',
-                  borderWidth: 2,
-                  backgroundColor: 'rgb(54, 162, 235)',
-                  data: [2, 1, 1, 2],
-                },
-                {
-                  type: 'line',
-                  label: 'Story Points',
-                  backgroundColor: 'rgb(255, 99, 132)',
-                  data: [7, 2, 7, 10],
+                  data: userData.storyPoints,
                   borderColor: 'rgb(255, 99, 132)',
                   borderWidth: 4,
                 },
