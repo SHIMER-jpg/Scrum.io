d5e6d8d2c67c453ec03f442fd6862dc3ba244617
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-09-08 15:35:53 -0300

    Ahora los graficos de tareas segun priorizacion y reporte de proyecto usan datos reales de la bd, el sprint report se agrego con datos hardcodeados

--- client/src/components/StatisticCard/StatisticCard.js
+++ client/src/components/StatisticCard/StatisticCard.js
@@ -1,20 +1,70 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { React, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
+// componentes charts de chartjs
 import { Bar } from "react-chartjs-2";
 import {Line} from 'react-chartjs-2';
 
 import styles from "./StatisticCard.module.css";
 
-export default function StatisticCard({ graphType }) {
-
-  let project = useSelector((state) => state.managerView.project);
-  let tasks = useSelector((state) => state.managerView.tasks);
+export default function StatisticCard({graphType, tasks, project}) {
+  const [graphData, setGraphData] = useState({
+    byStoryPoints: [],
+    byTasks: [],
+  })
 
   if(tasks.length > 0) {
     var storyPoints = tasks.map(t => t.storyPoints).reduce((a,b) => a + b)
   }
 
+  useEffect(() => {
+    if(tasks.length > 0){
+      setGraphData({
+        ...graphData,
+        byStoryPoints: charDataByStoryPoints(),
+        byTasks: charDataByTasks(),
+      })
+    }
+  },[tasks])
+
+  // funcion para setear la data por StoryPoints
+  function charDataByStoryPoints(){
+    if(graphType === "Tasks Priorization Chart"){
+      return[
+        tasks.filter(t => t.priorization === "Easy Win").map(t => t.storyPoints).reduce((points, a) => points +a),
+        tasks.filter(t => t.priorization === "Strategic Initiative").map(t => t.storyPoints).reduce((points, a) => points + a),
+        tasks.filter(t => t.priorization === "Worth Pursuing").map(t => t.storyPoints).reduce((points, a) => points + a),
+        tasks.filter(t => t.priorization === "Deprioritize").map(t => t.storyPoints).reduce((points, a) => points + a),
+      ]
+    }
+    else if(graphType === "Project Report"){
+      return[
+        tasks.filter(t => t.status === "Pending").map(t => t.storyPoints).reduce((points, a) => points + a),
+        tasks.filter(t => t.status === "In progress").map(t => t.storyPoints).reduce((points, a) => points + a),
+        tasks.filter(t => t.status === "Testing").map(t => t.storyPoints).reduce((points, a) => points + a),
+        tasks.filter(t => t.status === "Completed").map(t => t.storyPoints).reduce((points, a) => points + a),
+      ]
+    }
+  }
+  // funcion para setear la data por tasks
+  function charDataByTasks(){
+    if(graphType === "Tasks Priorization Chart"){
+      return [
+        tasks.filter(t => t.priorization === "Easy Win").length,
+        tasks.filter(t => t.priorization === "Strategic Initiative").length,
+        tasks.filter(t => t.priorization === "Worth Pursuing").length,
+        tasks.filter(t => t.priorization === "Deprioritize").length,
+      ]
+    }
+    else if(graphType === "Project Report"){
+      return[
+        tasks.filter(t => t.status === "Pending").length,
+        tasks.filter(t => t.status === "In progress").length,
+        tasks.filter(t => t.status === "Testing").length,
+        tasks.filter(t => t.status === "Completed").length,
+      ]
+    }
+  }
 
   return (
     <div className={styles.conteiner}>
@@ -24,28 +74,94 @@ export default function StatisticCard({ graphType }) {
         </div>
         <div className={styles.graph}>
 
-          {graphType === "Tasks Complexity Chart" ?
+          {graphType ? 
+            graphType === "Tasks Priorization Chart" ?
 
+              <Bar className={styles.chart}
+                data = {{
+                  labels: ["Easy Win", "Strategic Initiatives", "Worth Persuing Later", "Deprioritize"],
+                  datasets: [{
+                    axis: 'y',
+                    label: 'Story Points',
+                    data: graphData.byStoryPoints,
+                    fill: false,
+                    backgroundColor: ['#A12464'],
+                    borderColor: ['#A12464'],
+                    borderWidth: 1
+                  },
+                  {
+                    axis: 'y',
+                    label: 'Tasks',
+                    data: graphData.byTasks,
+                    fill: false,
+                    backgroundColor: ['#7BEFFF'],
+                    borderColor: ['#7BEFFF'],
+                    borderWidth: 1
+                  }]
+                }}
+                option={{
+                  maintainAspectRatio: false,
+                  scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                  }
+                }}
+
+              />
+            : graphType === "Project Report" ?
+              <Bar className={styles.chart}
+                data = {{
+                  labels: ["Pending", "In progress", "Testing", "Completed"],
+                  datasets: [{
+                    axis: 'y',
+                    label: 'Story Points',
+                    data: graphData.byStoryPoints,
+                    fill: false,
+                    backgroundColor: ['#A12464'],
+                    borderColor: ['#A12464'],
+                    borderWidth: 1
+                  },
+                  {
+                    axis: 'y',
+                    label: 'Tasks',
+                    data: graphData.byTasks,
+                    fill: false,
+                    backgroundColor: ['#7BEFFF'],
+                    borderColor: ['#7BEFFF'],
+                    borderWidth: 1
+                  }]
+                }}
+                option={{
+                  maintainAspectRatio: false,
+                  scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                  }
+                }}
+
+              />
+            : graphType === "Sprint Report" ?
             <Bar className={styles.chart}
               data = {{
-                labels: ["Easy Win", "Strategic Initiatives", "Worth Persuing Later", "Desprioritize"],
+                labels: ["Pending", "In progress", "Testing", "Completed"],
                 datasets: [{
                   axis: 'y',
-                  label: 'My First Dataset',
-                  data: [65, 59, 80, 81, 56, 55, 40],
+                  label: 'Story Points',
+                  data: [412, 550, 322, 478],
                   fill: false,
-                  backgroundColor: [
-                    '#8EFF7B',
-                    '#7BEFFF',
-                    '#FFA53C',
-                    '#FF6868',
-                  ],
-                  borderColor: [
-                    '#8EFF7B',
-                    '#7BEFFF',
-                    '#FFA53C',
-                    '#FF6868',
-                  ],
+                  backgroundColor: ['#A12464'],
+                  borderColor: ['#A12464'],
+                  borderWidth: 1
+                },
+                {
+                  axis: 'y',
+                  label: 'Tasks',
+                  data: [10, 5, 6, 8],
+                  fill: false,
+                  backgroundColor: ['#7BEFFF'],
+                  borderColor: ['#7BEFFF'],
                   borderWidth: 1
                 }]
               }}
@@ -59,42 +175,42 @@ export default function StatisticCard({ graphType }) {
               }}
 
             />
-          : graphType === "BurnDown Chart" ?
-            <Line className={styles.chart}
-            data={{
-              labels: ['0','1', '2', '3', '4', '5', '6', '7'],
-              datasets: [
-              {
-                label: 'Actual development',
-                data: [21, 16, 12, 9, 7, 5, 2, 0],
-                borderColor:['#a12464']
-              },
-              {
-                label: 'Ideal development',
-                data: [21,18,15,12,9,6,3,0],
-                borderColor:['#7BEFFF']
-              }
-            ]
-            }}
-            option={{
-              maintainAspectRatio: false,
-              scales: {
-                y: {
-                    beginAtZero: true
+          :
+            graphType === "BurnDown Chart" ?
+              <Line className={styles.chart}
+              data={{
+                labels: ['0','1', '2', '3', '4', '5', '6', '7'],
+                datasets: [
+                {
+                  label: 'Actual development',
+                  data: [21, 16, 12, 9, 7, 5, 2, 0],
+                  borderColor:['#a12464']
+                },
+                {
+                  label: 'Ideal development',
+                  data: [21,18,15,12,9,6,3,0],
+                  borderColor:['#7BEFFF']
                 }
-              }
-            }}
-            />
+              ]
+              }}
+              option={{
+                maintainAspectRatio: false,
+                scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+                }
+              }}
+              />
+            :
+            <>
+            </>
           :
           <>
           </>
           }
         </div>
         <div className={styles.description}>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-          </p>
         </div>
       </div>
     </div>
--- client/src/components/Statistics/Statistics.js
+++ client/src/components/Statistics/Statistics.js
@@ -1,14 +1,33 @@
-import { React, useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { React, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import styles from "./Statistics.module.css";
+
+// acciones
+import {
+  getProjectById,
+  getTasksByProject,
+} from "../../redux/ManagerView/actions";
 
 // components
 import StatisticCard from "../StatisticCard/StatisticCard";
 import StatisticDeveloper from "../StatisticDeveloper/StatisticDeveloper";
 
-export default function Statistics() {
+export default function Statistics(props) {
+  const project = useSelector((state) => state.managerView.project);
+  const tasks = useSelector((state) => state.managerView.tasks);
+  const dispatch = useDispatch();
 
+  // si no hay valores en los estados globales, ejecuta las acciones de ManagerView que los pedirian
+  useEffect(()=>{
+    if(Object.keys(project).length === 0){
+      dispatch(getProjectById(props.match.params.projectId));
+    }
+    if(tasks.length === 0){
+      dispatch(getTasksByProject(props.match.params.projectId));
+    }
+  },[]);
   return (
     <div className={styles.conteiner}>
       <div className={styles.header}>
@@ -16,8 +35,10 @@ export default function Statistics() {
       </div>
       <div className={styles.body}>
         <div className={styles.cardsList}>
-          <StatisticCard graphType={"BurnDown Chart"}/>
-          <StatisticCard graphType={"Tasks Complexity Chart"}/>
+          <StatisticCard tasks={tasks} project={project} graphType={"BurnDown Chart"}/>
+          <StatisticCard tasks={tasks} project={project} graphType={"Tasks Priorization Chart"}/>
+          <StatisticCard tasks={tasks} project={project} graphType={"Sprint Report"}/>
+          <StatisticCard tasks={tasks} project={project} graphType={"Project Report"}/>
         </div>
         <div className={styles.specialCard}>
           <StatisticDeveloper />
