41577b85fa4d9437afe5554ad6bbc4a229cf0edd
Rocio <rocioreginaottavianelli@gmail.com>
2021-09-09 16:37:16 -0300

    cambios menores

--- client/src/components/StatisticDeveloper/StatisticDeveloper.js
+++ client/src/components/StatisticDeveloper/StatisticDeveloper.js
@@ -11,12 +11,18 @@ import PopperHelp from "../PopperHelp/PopperHelp.js";
 import { useSearch } from "../../hooks/useSearch";
 
 export default function StatisticDeveloper(props) {
+  //---STATES----------------------------------------
+  const [view, setView] = useState("days");
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [data, setData] = useState([]);
+  const [isSelectUsersOpen, setIsSelectUsersOpen] = useState(true);
+
   const project = useSelector((state) => state.managerView.project);
   const users = useSelector((state) => state.managerView.asignedUsers);
   const tasks = useSelector((state) => state.managerView.tasks);
-  console.log(project, "project");
+  // console.log(project, project.creationDate, "project");
   console.log(users, "users");
-  console.log(tasks, "tasks");
+  // console.log(tasks, "tasks");
 
   const userTasks = tasks.filter((t) => t.asignedTo === "613274bb1a9c7e2b10cfe1c1");
   console.log(userTasks, "userTasks")
@@ -24,14 +30,10 @@ export default function StatisticDeveloper(props) {
   const completedUserTasks = userTasks.filter((t) => t.status === "Completed");
   console.log(completedUserTasks, "completed")
 
-  const userData = getData(project.creationDate, "2021-01-26T22:45:54.000Z", completedUserTasks);
+  const userData = getData(project.creationDate, moment(), completedUserTasks);
   console.log(userData, "userdata");
 
   const labels = getLabels(project.requiredDate, project.creationDate);
-
-  const [selectedUsers, setSelectedUsers] = useState([]);
-  const [data, setData] = useState([]);
-  const [isSelectUsersOpen, setIsSelectUsersOpen] = useState(false);
 
   // const [query, setQuery, filteredUsers] = useSearch(users);
 
@@ -53,11 +55,11 @@ export default function StatisticDeveloper(props) {
   function getData(initialDate, actualDate, tasks){  //creates a totalizer data object
     var days = calculateDays(actualDate, initialDate);
     var data = {quantity: [], storyPoints: []};
-    for(let i=0; i<days; i++){ //initializing both arrays in 0 for each position
+    for(let i=0; i<=days; i++){ //initializing both arrays in 0 for each position
       data.quantity[i] = 0;
       data.storyPoints[i] = 0;
     }
-    console.log(data.quantity, data.storyPoints, "weas")
+    console.log(data.quantity, data.storyPoints, "data")
     tasks.forEach((t) =>    //mapping user tasks
       {
         console.log(calculateDays(t.completedDate, initialDate))
@@ -69,21 +71,28 @@ export default function StatisticDeveloper(props) {
   }
 
   function handleAddUser(user){
-    if (!selectedUsers.includes(user._id)) {
+    if (!selectedUsers.includes(user.userId)) {
       setSelectedUsers({
         ...selectedUsers,
-        selectedUsers: [...selectedUsers, user._id]
+        selectedUsers: [...selectedUsers, user.userId]
       });
     }
+    console.log(user.userId);
+    console.log("agregue usuario");
     // setQuery("");
   }
 
   function handleRemoveUser(user){
     setSelectedUsers({
       ...selectedUsers,
-      selectedUsers: selectedUsers.filter((u) => u !== user._id)
+      selectedUsers: selectedUsers.filter((u) => u !== user.userId)
     })
   }
+
+  useEffect(() => {
+    console.log("cambiaron los usuarios")
+  }, [selectedUsers])
+
   // console.log(filteredUsers, "filteredUsers");
   //-------RETURN-------------------------------------
   return (
@@ -101,17 +110,17 @@ export default function StatisticDeveloper(props) {
               datasets: [{
                   type: 'bar',
                   label: 'Completed Tasks',
-                  borderColor: 'rgb(54, 162, 235)',
+                  borderColor: 'rgb(142, 255, 123)',
                   borderWidth: 2,
-                  backgroundColor: 'rgb(54, 162, 235)',
+                  backgroundColor: 'rgb(142, 255, 123)',
                   data: userData.quantity,
                 },
                 {
                   type: 'line',
                   label: 'Story Points',
-                  backgroundColor: 'rgb(255, 99, 132)',
+                  backgroundColor: 'rgb(142, 255, 123)',
                   data: userData.storyPoints,
-                  borderColor: 'rgb(255, 99, 132)',
+                  borderColor: 'rgb(142, 255, 123)',
                   borderWidth: 4,
                 },
               ],
@@ -152,7 +161,7 @@ export default function StatisticDeveloper(props) {
                 key={user._id}
                 className={styles.modalUser}
               >
-                <p>{user.name}</p>
+                <p>{user.user.name}</p>
               </article>
             ))
           ) : (
@@ -161,10 +170,10 @@ export default function StatisticDeveloper(props) {
         </div>
         <div className={styles.addedUsers}>
           {users
-            .filter((user) => selectedUsers.includes(user._id))
+            .filter((user) => selectedUsers.includes(user.userId))
             .map((user) => {
-              <article key={user._id} className={styles.addedUsersCard}>
-                <p>{user.name.split(" ")[0]}</p>
+              <article key={user.userId} className={styles.addedUsersCard}>
+                <p>{user.user.name.split(" ")[0]}</p>
                 <button onClick={() => handleRemoveUser(user)}>
                   x
                 </button>
