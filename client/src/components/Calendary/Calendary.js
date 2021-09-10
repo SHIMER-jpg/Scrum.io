f97463381563fd5f6fd10fe818f87d29b6859472
cuellojuancruz <cuellojuancruz11@gmail.com>
2021-09-10 14:36:38 -0300

    prueba del calendario al clickear en una fecha

--- client/src/components/Calendary/Calendary.js
+++ client/src/components/Calendary/Calendary.js
@@ -1,11 +1,34 @@
+import React, { useState } from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
 
+
+
+// Modal.setAppElement("#root");
 
 const Calendary = function(){
 
-    return(
-        <h1>holaaa</h1>
-    )
+
+  function handleDateClick(){
+    alert("hola")
+  }
+
+
+ 
+
+  return(
+    <FullCalendar
+      plugins={[ dayGridPlugin, interactionPlugin  ]}
+      initialView="dayGridMonth"
+      weekends={false}
+      dateClick={handleDateClick}
+    />
+  )
 }
+  
+    
+
 
 
 export default Calendary
