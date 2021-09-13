5b962d28a0b1063bc51b5898a3970c127e51ea07
Lucero Amaolo <lamaolo.m@gmail.com>
2021-09-12 23:36:29 -0300

    Cors seteado en *

--- api/src/app.js
+++ api/src/app.js
@@ -26,7 +26,7 @@ app.use(morgan("dev"));
 app.use(formData.parse());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
