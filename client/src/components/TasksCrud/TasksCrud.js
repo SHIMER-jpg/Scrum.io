4ae79612b3470945fdabc8315304ed295856be0f
Fabrizio Tessaro <fabriziotessaro02@gmail.com>
2021-09-14 18:55:59 -0300

    Nuevo componente TasksCrud creado con su respectivo .module.css

---
+++ client/src/components/TasksCrud/TasksCrud.js
@@ -0,0 +1,11 @@
+import { React, useEffect, useState, useCallback } from 'react';
+import styles from './TasksCrud.module.css';
+
+export default function TasksCrud(){
+    
+    return(
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
