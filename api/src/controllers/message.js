d77fb01ed1400fc3d81d8e2d35357f26c3fa2ac7
Rocio <rocioreginaottavianelli@gmail.com>
2021-09-15 15:46:07 -0300

    controlador de get message eliminado, era redundante

--- api/src/controllers/message.js
+++ api/src/controllers/message.js
@@ -2,25 +2,16 @@ const Message = require("../models/Message");
 const Chat = require("../models/Chat");
 const mongoose = require("mongoose");
 
-const getMessagesByChatId = async (req, res, next) => {
-  try {
-    const data = await Message.model.find({ chatId: req.body.chatId })
-    res.status(200).json(data);
-  } catch (error) {
-    next(error);
-  }
-};
-
 const postMessage = async (req, res, next) => {
   try{
     const newMessage = new Message.model({
       userId: mongoose.Types.ObjectId(req.body.userId),
-      chatId: mongoose.Types.ObjectId(req.body.chatId),
+      projectId: mongoose.Types.ObjectId(req.body.projectId),
       content: req.body.content,
     });
     await newMessage.save();
     const chat = await Chat.model.findOne({
-      _id: mongoose.Types.ObjectId(req.body.chatId),
+      projectId: mongoose.Types.ObjectId(req.body.projectId),
     });
     chat.messageIds.push(newMessage._id);
     chat.save();
@@ -31,6 +22,5 @@ const postMessage = async (req, res, next) => {
 };
 
 module.exports = {
-  getMessagesByChatId,
   postMessage,
 };
