d4e5e2487fd607a01f38ca95287872ecb31c0185
Rocio <rocioreginaottavianelli@gmail.com>
2021-09-15 15:47:07 -0300

    eliminada la ruta de get message

--- api/src/routes/message.js
+++ api/src/routes/message.js
@@ -2,12 +2,9 @@ const { Router } = require("express");
 
 const message = Router();
 const {
-  getMessagesByChatId,
   postMessage,
 } = require("../controllers/message.js");
 
-//Gets all messages from a chat
-message.get("/getMessages", getMessagesByChatId);
 //Post a new message on a chat
 message.post("/newMessage", postMessage);
 
