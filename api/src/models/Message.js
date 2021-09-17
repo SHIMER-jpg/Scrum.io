4f56037b8148a8b2e7ba844500b86e8572307e5c
federico666 <fefosonic1@gmail.com>
2021-09-16 22:08:31 -0300

    error con estilos, componente ya funcionando

--- api/src/models/Message.js
+++ api/src/models/Message.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const messageSchema = new mongoose.Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  content: {
-    type: String,
-    required: true,
+const messageSchema = new mongoose.Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    content: {
+      type: String,
+      required: true,
+    },
+
+    projectId: { type: Schema.Types.ObjectId, ref: "Project", required: true },
   },
-  timeStamp: { type: Date, default: Date.now() },
-  projectId: { type: Schema.Types.ObjectId, ref: "Project", required: true },
-});
+  { timestamps: true }
+);
 
 module.exports = {
   schema: messageSchema,
--- client/src/views/Chat/Chat.js
+++ client/src/views/Chat/Chat.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import moment from "moment";
 import {
@@ -8,17 +8,22 @@ import {
 } from "../../redux/Chat/actions";
 import styles from "./Chat.module.css";
 import { CgClose } from "react-icons/cg";
+import { FaTelegramPlane } from "react-icons/fa";
+import useTimeAgo from "../../hooks/useTimeAgo";
 
-export function Chat({buttonOpen, setButtonOpen}) {
+export function Chat({ buttonOpen, setButtonOpen }) {
   const projectId = useSelector((state) => state.managerView.project._id);
   const userId = useSelector((state) => state.app.loggedUser._id);
   const chatMap = useSelector((state) => state.chatInfo.messages);
   const socket = useSelector((state) => state.app.socket);
-  console.log(projectId);
 
   const dispatch = useDispatch();
 
   const [messages, setMessages] = useState("");
+
+  const container = useRef(null);
+
+  var firstTime = true;
 
   useEffect(() => {
     if (projectId) {
@@ -31,63 +36,85 @@ export function Chat({buttonOpen, setButtonOpen}) {
   }, [projectId]);
 
   const handleNewMessage = (message) => {
-    console.log(message);
+    if (firstTime) {
+      container.current.scrollTop = container.current.scrollHeight;
+      firstTime = false;
+    } else if (
+      container.current.scrollTop + container.current.clientHeight ===
+      container.current.scrollHeight
+    ) {
+      container.current.scrollTop = container.current.scrollHeight;
+    }
+
     if (message.projectId === projectId && message.userId !== userId) {
       dispatch(updateMessage(message));
     }
   };
 
   function handleSubmit(e) {
-    e.preventDefault();
-    dispatch(createMessage(userId, projectId, messages));
-  }
-
-  function calculateTime(time) {
-    time = moment(time);
-    var now = moment().subtract(4, "hours");
-    if(now.diff(time, "days") >= 1){
-      return now.diff(time, "days") + " days ago";
-    }
-    if(now.diff(time, "hours") >= 1){
-      return now.diff(time, "hours") + " hours ago";
-    }
-    if(now.diff(time, "minutes") >= 1){
-      return now.diff(time, "minutes") + " minutes ago";
+    if (messages !== "") {
+      e.preventDefault();
+      dispatch(createMessage(userId, projectId, messages));
+      setMessages("");
     }
   }
 
   return (
     <div className={styles.chat}>
-      <div className={styles.header}>
-        <label>Project Chat</label>
-        <button onClick={() => setButtonOpen(!buttonOpen)}><CgClose size={20}/></button>
+      <div className={styles.chatBox}>
+        <div className={styles.header}>
+          <label>Project Chat</label>
+          <button onClick={() => setButtonOpen(!buttonOpen)}>
+            <CgClose size={20} />
+          </button>
+        </div>
+        <div ref={container} className={styles.container}>
+          {chatMap !== undefined && chatMap.length !== 0
+            ? chatMap.map((message, i) => {
+                console.log(message, "hola soy el mensaje");
+                return <Message message={message} userId={userId} key={i} />;
+              })
+            : []}
+        </div>
+        <div className={styles.typing}>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+              value={messages}
+              placeholder="Write a message..."
+              onChange={(e) => setMessages(e.target.value)}
+            />
+            <button type="submit ">
+              {" "}
+              <FaTelegramPlane size={26} />{" "}
+            </button>
+          </form>
+        </div>
       </div>
-      <div className={styles.container}>
-        {chatMap !== undefined && chatMap.length !== 0
-          ? chatMap.map((message, i) => {
-              console.log(message, "hola soy el mensaje");
-              return (
-                <div key={i} className={styles.message}>
-                  <div className={styles.user}>
-                    <img src={message.userId.picture} />
-                    <div className={styles.text}>
-                      <div className={styles.info}>
-                        <p className={styles.name}> {message.userId.name.split(" ")[0]} </p>
-                        <p className={styles.time}> {calculateTime(message.timeStamp)} </p>
-                      </div>
-                      <p className={styles.content}>{message.content}</p>
-                    </div>
-                  </div>
-                </div>
-              );
-            })
-          : []}
-      </div>
-      <div className={styles.typing}>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input onChange={(e) => setMessages(e.target.value)} />
-          <button type="submit ">Send</button>
-        </form>
+    </div>
+  );
+}
+
+function Message({ message, userId }) {
+  console.log(message, "holii");
+  const timeAgo = useTimeAgo(new Date(message.createdAt), "short");
+
+  return (
+    <div className={styles.message}>
+      <div className={userId === message.userId._id ? styles.me : styles.user}>
+        {userId !== message.userId._id && <img src={message.userId.picture} />}
+        <div className={styles.messageBox}>
+          <div className={styles.info}>
+            <p className={styles.name}>
+              {" "}
+              {userId !== message.userId._id &&
+                message.userId.name.split(" ")[0]}{" "}
+            </p>
+            <p className={styles.time}> {timeAgo} </p>
+          </div>
+          <div className={styles.text}>
+            <p className={styles.content}>{message.content}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
